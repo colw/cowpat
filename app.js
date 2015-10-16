acd6d6a93761da78c145f3838e1b72cd4c94388b
colw <colw@outlook.com>
2015-10-16 18:01:49 +0200

    Tidy app server code. Remove redundant http object.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 /* Server */
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
-
-http.listen(app.get('port'), function() {
+app.listen(app.get('port'), function() {
   console.log("Serving NXWS Client at localhost:" + app.get('port'));
 });
