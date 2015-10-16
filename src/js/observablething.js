f6e613dda82f2b38bf7482fe3688338fc359f93d
colw <colw@outlook.com>
2015-10-16 23:28:25 +0200

    ObservableThing is now a class

--- src/js/observablething.js
+++ src/js/observablething.js
@@ -1,26 +1,25 @@
-var ObservableThing = function(thing) {
-  this.thing = thing;
-  this.onChangeListener = null;
-};
+export default class ObservableThing {
+	constructor(thing) {
+	  this.thing = thing;
+    this.onChangeListener = null;
+	}
 
-ObservableThing.prototype.set = function(newValue) {
-  this.thing = newValue;
-  this.notify();
-};
+	set(newValue) {
+	  this.thing = newValue;
+	  this.notify();
+	}
+	
+	get() {
+	  return this.thing;
+	}
 
-ObservableThing.prototype.get = function() {
-  return this.thing;
-};
+	setChangeListener(listener) {
+	  this.onChangeListener = listener;
+	}
 
-ObservableThing.prototype.setChangeListener = function(listener) {
-  this.onChangeListener = listener;
-};
-
-ObservableThing.prototype.notify = function() {
-  if (this.onChangeListener !== null) {
-    this.onChangeListener();
-  }
-};
-
-
-export default ObservableThing;
+	notify() {
+	  if (this.onChangeListener !== null) {
+	    this.onChangeListener();
+	  }
+	}
+}
