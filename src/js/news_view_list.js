36c02c9b58a342c52f23714848698babddd7f219
colw <colw@outlook.com>
2015-10-16 15:40:13 +0200

    Remove lib dependencies. Use node modules instead.

--- src/js/news_view_list.js
+++ src/js/news_view_list.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import moment from 'moment';
 
 import {FormatURLMixin, SetIntervalMixin} from './news_view_mixins.js';
 import {newsItems, numberOfReaders, sourceList, getStateFromNewsItems, getStateFromNumberOfReaders, getStateFromSourceList} from './client_model';
-
 import {NewsInfo, NewsCow, NewsSearchBar, NewsTagList} from './news_view_header';
 import {HowCow} from './news_view_about';
 
