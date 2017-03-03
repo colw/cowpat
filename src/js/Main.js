6c055c6724c8f139612f0ffcccdaae69ca9b7db8
colw <colw@outlook.com>
2017-03-03 15:23:05 +0100

    Clean fetch data

--- src/js/Main.js
+++ src/js/Main.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'whatwg-fetch';
-import _ from 'lodash';
+import { uniqBy } from 'lodash';
 
 import App from './App'
 import { createStore } from './store';
@@ -10,22 +10,26 @@ import { createStore } from './store';
 import "../index.html";
 import './favicons.js'
 
+const constructItem = (item) => {
+  item.tags = JSON.parse(item.tags);
+  item.date = new Date(item.date);
+  return item;
+}
+
+const orderItems = (data) => {
+	data.items = data.items.map(constructItem)
+	data.items = uniqBy(data.items, 'itemID') || []
+	// data.items = _.sortBy(data.items, 'date');
+	data.items.reverse();
+	return data;
+}
+
 const fetchItems = (fetchTag, oldestID) => {
 	fetchTag = fetchTag || '';
 	oldestID = oldestID || null;
 	return fetch(`${API_URL}${fetchTag ? '/items/' + fetchTag + '' : ''}${oldestID ? '?&oldest=' + oldestID: ''}`)
 	  .then(res => res.json())
-	  .then(json => {
-	    json.items = json.items.map(x => {
-	      x.tags = JSON.parse(x.tags);
-	      x.date = new Date(x.date);
-	      return x;
-	    })
-			json.items = _.uniqBy(json.items, 'itemID') || []
-	  	// json.items = _.sortBy(json.items, 'date');
-	  	json.items.reverse();
-	    return json;
-	  })
+	  .then(orderItems)
 }
 
 const initialState = {
